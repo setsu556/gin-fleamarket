9ef59ce7b6b1f4b21ba11d5691a17d934897f9e1
setsu556 <qq5y5bsd@gmail.com>
2025-10-08 18:33:54 +0900

    アイテム管理機能を追加 (モデル、リポジトリ、サービス、コントローラー、およびルート設定)

--- main.go
+++ main.go
@@ -1,13 +1,27 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"gin-fleamarket/controllers"
+	"gin-fleamarket/models"
+	"gin-fleamarket/repositories"
+	"gin-fleamarket/services"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
+
+	items := []models.Item{
+		{ID: 1, Name: "商品１", Price: 1000, Description: "説明１", SoldOut: false},
+		{ID: 2, Name: "商品２", Price: 2000, Description: "説明２", SoldOut: true},
+		{ID: 3, Name: "商品３", Price: 3000, Description: "説明３", SoldOut: false},
+	}
+
+	itemRepository := repositories.NewItemMemoryRepository(items)
+	itemService := services.NewItemService(itemRepository)
+	itemController := controllers.NewItemController(itemService)
+
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
+	r.GET("/items", itemController.FindAll)
 	r.Run("localhost:8080") // 0.0.0.0:8080 でサーバーを立てます。
 }
