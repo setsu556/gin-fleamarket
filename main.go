8deca9c6e553169c63ddfe48ff398b3cff6d7438
setsu556 <qq5y5bsd@gmail.com>
2025-10-16 11:05:11 +0900

    アイテム更新機能を追加 (DTO、リポジトリ、サービス、コントローラー、およびルート設定)

--- main.go
+++ main.go
@@ -25,5 +25,6 @@ func main() {
 	r.GET("/items", itemController.FindAll)
 	r.GET("/items/:id", itemController.FindById)
 	r.POST("/items", itemController.Create)
+	r.PUT("/items/:id", itemController.Update)
 	r.Run("localhost:8080") // 0.0.0.0:8080 でサーバーを立てます。
 }
